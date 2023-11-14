f49fba43bb8047f17edaf9e63bd48046682ec02e
mhguo1 <mhguo1@gmail.com>
2023-11-14 15:41:20 -0500

    Update run_gangSTR.sh

--- STR_genotyping/run_gangSTR.sh
+++ STR_genotyping/run_gangSTR.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/sh
 
+#This code will run gangSTR v2.4. Users will need to supply arguments for the sample_id, sex, path to a bam/cram file, the STR reference bed file, and reference genome.
+
 ref="/ref/GRCh38_full_analysis_set_plus_decoy_hla.fa" #Reference genome
 
 str_ref="/project/jcreminslab/guomic_projects/str_test/str_ref_merge/gangstr.v13.polymorphic_w_eh.v5_offtarget.exp10_p05.top5_no_segdup.fast_2s.bed"
