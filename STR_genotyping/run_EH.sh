cd58d3e6afeedb8ae83d646330faffd3c4ecfdec
mhguo1 <mhguo1@gmail.com>
2023-11-14 16:26:19 -0500

    Update run_EH.sh

--- STR_genotyping/run_EH.sh
+++ STR_genotyping/run_EH.sh
@@ -1,1 +1,17 @@
 #!/usr/bin/sh
+
+#This code runs ExpansionHunter v5
+
+#For ExpansionHunter:
+ref="/ref/GRCh38_full_analysis_set_plus_decoy_hla.fa"
+bam=$1 #path to bam/cram
+sample=$2 #sample prefix/ID
+json="eh.v5_w_gangstr.v13.polymorphic.json"
+
+/project/jcreminslab/guomic_projects/software/ExpansionHunter-v5.0.0-linux_x86_64/bin/ExpansionHunter \
+--reads ${bam} \
+--reference ${ref} \
+--variant-catalog ${json} \
+--output-prefix ${sample}.eh_test \
+--analysis-mode streaming \
+--log-level warn
