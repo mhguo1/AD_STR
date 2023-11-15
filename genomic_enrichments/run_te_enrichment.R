c81605d565e35dcf17fd219ce4a84a384f29b387
mhguo1 <mhguo1@gmail.com>
2023-11-14 23:28:26 -0500

    Update run_te_enrichment.R

--- genomic_enrichments/run_te_enrichment.R
+++ genomic_enrichments/run_te_enrichment.R
@@ -64,103 +64,30 @@ te_list<-c("SVA","Alu","LINE2","LINE1","HERV")
 for(s in te_list){
 
     #Read in repeat masker file and subset for TE class of interest
-    dat.annot<-read.delim(paste0("/project/jcreminslab/guomic_projects/ad_str/eh_0822/enrichment/annotations/repeat_masker.",s, "_", d, "bp.hg38.bed"), header=F, sep="\t", stringsAsFactors = F)
+    dat.annot<-read.delim(paste0("/annotations/repeat_masker.",s, ".hg38.bed"), header=F, sep="\t", stringsAsFactors = F)
     dat.annot$str_id<-paste0(dat.annot$V1, "_", dat.annot$V2, "_", dat.annot$V3)
     dat.annot.se<-makeGRangesFromDataFrame(dat.annot, seqnames = "V1", start.field = "V2", end.field = "V3",keep.extra.columns=TRUE) #Make into GRanges format
       
-    #Run permutation testing
-    n<-0
-    
-    dat.summary<-data.frame(trial=seq(0,n,1), foreground_overlap=0, background_overlap=0,stringsAsFactors = F)
-    for(i in 1:nrow(dat.summary)){
-      if(dat.summary[i,]$trial==0){
-        dat.random.se<-dat.annot.se
-      }else{
-       # dat.random.se<-randomizeRegions(dat.annot.se, genome="hg38", per.chromsome=T)
-        dat.random.se<-randomizeRegions(dat.annot.se, genome="hg38", per.chromsome=F, mask=dat.segdup.gr)
-      }
-      dat.summary[i,]$foreground_overlap<-length(unique(data.frame(subsetByOverlaps(dat.foreground.se, dat.random.se, type="any"), stringsAsFactors = F)$str_id)) #
-      dat.summary[i,]$background_overlap<-length(unique(data.frame(subsetByOverlaps(dat.background.se, dat.random.se, type="any"), stringsAsFactors = F)$str_id))
-    }
-    dat.summary$foreground_prop<-dat.summary$foreground_overlap/nrow(dat.foreground)
-    dat.summary$background_prop<-dat.summary$background_overlap/nrow(dat.background)
-    dat.summary$te<-s
-    dat.summary$dist<-d
-    dat.all<-rbind(dat.all, dat.summary)
-  }
-}
-
-
-#run ENCODE tissue enrichment for hippocampus
-dat.all<-data.frame(trial=numeric(), foreground_overlap=numeric(), background_overlap=numeric(), 
-                    foreground_prop=numeric(), background_prop=numeric(), 
-                    tissue=character(),stringsAsFactors = F)
-#encode_list<-"ENCFF886QLM"
-encode_list<-c("h3k27ac","h3k27me3","h3k36me3","h3k4me1","h3k4me3","h3k9ac","h3k9me3", "h3k4me3_h3k27ac")
-for(s in encode_list){
-  dat.annot<-read.delim(paste0("/project/jcreminslab/guomic_projects/ad_str/eh_0822/annot/encode/", s, "/hippocampus.", s, ".encode.merged.bed"), header=F, sep="\t", stringsAsFactors = F)
-  
+    #Generate temporary annotation file for TE class
+    dat.annot<-subset(dat.chromhmm, V4==c & V1%in%paste0("chr", seq(1,22)), select=-c(V4))
+    c<-gsub("\\/", "_",c)
     dat.annot$str_id<-paste0(dat.annot$V1, "_", dat.annot$V2, "_", dat.annot$V3)
     dat.annot.se<-makeGRangesFromDataFrame(dat.annot, seqnames = "V1", start.field = "V2", end.field = "V3",keep.extra.columns=TRUE) #Make into GRanges format
     
-
-    #Run permutation testing
-    n<-0
+    #Calculate overlaps
+    dat.summary[i,]$foreground_overlap<-length(unique(data.frame(subsetByOverlaps(dat.foreground.se, dat.annot.se, type="any"), stringsAsFactors = F)$str_id)) #
+    dat.summary[i,]$background_overlap<-length(unique(data.frame(subsetByOverlaps(dat.background.se, dat.annot.se, type="any"), stringsAsFactors = F)$str_id))
     
-    dat.summary<-data.frame(trial=seq(0,n,1), foreground_overlap=0, background_overlap=0,stringsAsFactors = F)
-    for(i in 1:nrow(dat.summary)){
-      if(dat.summary[i,]$trial==0){
-        dat.random.se<-dat.annot.se
-      }else{
-        #dat.random.se<-randomizeRegions(dat.annot.se, genome="hg38", per.chromsome=F)
-        dat.random.se<-randomizeRegions(dat.annot.se, genome="hg38", per.chromsome=F, mask=dat.segdup.gr)
-      }
-      dat.summary[i,]$foreground_overlap<-length(unique(data.frame(subsetByOverlaps(dat.foreground.se, dat.random.se, type="any"), stringsAsFactors = F)$str_id)) #
-      dat.summary[i,]$background_overlap<-length(unique(data.frame(subsetByOverlaps(dat.background.se, dat.random.se, type="any"), stringsAsFactors = F)$str_id))
-    }
-    dat.summary$foreground_prop<-dat.summary$foreground_overlap/nrow(dat.foreground)
-    dat.summary$background_prop<-dat.summary$background_overlap/nrow(dat.background)
-    dat.summary$tissue<-s
+    dat.summary$foreground_prop<-dat.summary$foreground_overlap/nrow(dat.foreground) #Proportion of AD STRs overlapping TE class
+    dat.summary$background_prop<-dat.summary$background_overlap/nrow(dat.background) #Proportion of all STRs tested overlapping TE class
+
+    dat.summary$chromhmm<-c
+    dat.summary$tissue_id<-s
     dat.all<-rbind(dat.all, dat.summary)
-}
-write.table(dat.all, "eh_v5.dbscan_eps2.cases.E071_histone_enrichment.txt", row.names = F, col.names = T, sep="\t", quote=F)
-
-
-
-#run H3K9me3 tissue enrichment
-dat.all<-data.frame(trial=numeric(), foreground_overlap=numeric(), background_overlap=numeric(), 
-                    foreground_prop=numeric(), background_prop=numeric(), 
-                    encode_id=character(),stringsAsFactors = F)
-#encode_list<-list.files("/project/jcreminslab/guomic_projects/ad_str/encode/h3k9me3/test", pattern="p0.9999.merge20kb_domain100kb.bed")
-encode_list<-list.files("/project/jcreminslab/guomic_projects/ad_str/encode/h3k9me3/test", pattern="hippocampus")
-encode_list<-encode_list[grepl(".rseg", encode_list)]
-encode_list<-encode_list[grepl(".bed", encode_list)]
-
-for(s in encode_list){
-  dat.annot<-read.delim(paste0("/project/jcreminslab/guomic_projects/ad_str/encode/h3k9me3/test/",s), header=F, sep="\t", stringsAsFactors = F)
-#  dat.annot<-read.delim(paste0("/project/jcreminslab/guomic_projects/ad_str/eh_0822/annot/encode/",s), header=F, sep="\t", stringsAsFactors = F)
-  
-  
-  dat.annot$str_id<-paste0(dat.annot$V1, "_", dat.annot$V2, "_", dat.annot$V3)
-  dat.annot.se<-makeGRangesFromDataFrame(dat.annot, seqnames = "V1", start.field = "V2", end.field = "V3",keep.extra.columns=TRUE) #Make into GRanges format
-  
-  #Run permutation testing
-  n<-0
-  dat.summary<-data.frame(trial=seq(0,n,1), foreground_overlap=0, background_overlap=0,stringsAsFactors = F)
-  for(i in 1:nrow(dat.summary)){
-    if(dat.summary[i,]$trial==0){
-      dat.random.se<-dat.annot.se
-    }else{
-      dat.random.se<-randomizeRegions(dat.annot.se, genome="hg38", per.chromsome=F, mask=dat.segdup.gr)
-    }
-    dat.summary[i,]$foreground_overlap<-length(unique(data.frame(subsetByOverlaps(dat.foreground.se, dat.random.se, type="any"), stringsAsFactors = F)$str_id)) #
-    dat.summary[i,]$background_overlap<-length(unique(data.frame(subsetByOverlaps(dat.background.se, dat.random.se, type="any"), stringsAsFactors = F)$str_id))
   }
-  dat.summary$foreground_prop<-dat.summary$foreground_overlap/nrow(dat.foreground)
-  dat.summary$background_prop<-dat.summary$background_overlap/nrow(dat.background)
-  dat.summary$encode_id<-s
-  dat.all<-rbind(dat.all, dat.summary)
 }
+
+
 
 
 #Calculate p-values using Fisher's exact test
@@ -177,4 +104,4 @@ for(i in 1:nrow(dat.all)){
   }
 }
 dat.all<-dat.all[order(dat.all$pval),]
-write.table(dat.all, "eh_v5.dbscan_eps2.cases_recurrent1.all_tissues_chromhmm_enrichment.txt", row.names = F, col.names = T, sep="\t", quote=F)
+write.table(dat.all, "eh_v5.dbscan_eps2.cases_recurrent1.te_enrichment.txt", row.names = F, col.names = T, sep="\t", quote=F)
